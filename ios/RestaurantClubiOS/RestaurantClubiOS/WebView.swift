5252c6ad02e492bed8f531153900e757c95905a6
Brendan Pinder <BrendanPinder@MacBook-Pro-2.local>
2025-12-10 11:42:56 -0500

    location permissions request added for nearby restaurants

--- ios/RestaurantClubiOS/RestaurantClubiOS/WebView.swift
+++ ios/RestaurantClubiOS/RestaurantClubiOS/WebView.swift
@@ -7,6 +7,45 @@
 
 import SwiftUI
 import WebKit
+import CoreLocation
+
+// MARK: - Location Permission Helper
+
+class LocationPermissionManager: NSObject, CLLocationManagerDelegate {
+    static let shared = LocationPermissionManager()
+    
+    private let manager = CLLocationManager()
+    
+    private override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+    }
+    
+    func requestWhenInUseIfNeeded() {
+        let status: CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *) {
+            status = manager.authorizationStatus
+        } else {
+            status = CLLocationManager.authorizationStatus()
+        }
+        
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            break  // already decided (authorized / denied / restricted)
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        // Optional: you can log here for debugging if you want
+        // print("Location auth changed: \(manager.authorizationStatus.rawValue)")
+    }
+}
+
+// MARK: - Inspectable WebView
 
 class InspectableWebView: WKWebView {
     override init(frame: CGRect, configuration: WKWebViewConfiguration) {
@@ -26,6 +65,8 @@ class InspectableWebView: WKWebView {
     }
 }
 
+// MARK: - SwiftUI Wrapper
+
 struct WebView: UIViewRepresentable {
     let url: URL
     
@@ -36,6 +77,9 @@ struct WebView: UIViewRepresentable {
         
         let webView = InspectableWebView(frame: .zero, configuration: configuration)
         webView.scrollView.contentInsetAdjustmentBehavior = .never
+        
+        // Request location permission when the webview is created
+        LocationPermissionManager.shared.requestWhenInUseIfNeeded()
         
         return webView
     }
